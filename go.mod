805a562d03e6a41f0b581e7a8d2c65acf39c3a33
x-color <36035885+x-color@users.noreply.github.com>
2021-05-16 14:46:31 +0900

    Add files

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/x-color/cfn-doc
+
+go 1.16
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"flag"
+	"fmt"
+	"html/template"
+	"os"
+	"sort"
+
+	"gopkg.in/yaml.v2"
+)
+
+type resource struct {
+	ID   string
+	Type string `yaml:"Type,omitempty"`
+}
+
+type output struct {
+	ID          string
+	Description string `yaml:"Description,omitempty"`
+}
+
+type parameter struct {
+	ID          string
+	Description string `yaml:"Description,omitempty"`
+	Type        string `yaml:"Type,omitempty"`
+	Default     string `yaml:"Default,omitempty"`
+}
+
+type cfnTemplate struct {
+	Description string               `yaml:"Description,omitempty"`
+	Parameters  map[string]parameter `yaml:"Parameters,omitempty"`
+	Resources   map[string]resource  `yaml:"Resources,omitempty"`
+	Outputs     map[string]output    `yaml:"Outputs,omitempty"`
+}
+
+type templateValue struct {
+	Filename    string
+	Description string
+	Parameters  []parameter
+	Resources   []resource
+	Outputs     []output
+}
+
+//go:embed doc.template
+var docTemplate string
+
+func newTemplateValue(filename string, raw cfnTemplate) templateValue {
+	temp := templateValue{
+		Filename:    filename,
+		Description: raw.Description,
+		Parameters:  []parameter{},
+		Resources:   []resource{},
+		Outputs:     []output{},
+	}
+
+	for id, param := range raw.Parameters {
+		param.ID = id
+		temp.Parameters = append(temp.Parameters, param)
+	}
+	sort.Slice(temp.Parameters, func(i, j int) bool {
+		return temp.Parameters[i].ID < temp.Parameters[j].ID
+	})
+
+	for id, rs := range raw.Resources {
+		rs.ID = id
+		temp.Resources = append(temp.Resources, rs)
+	}
+	sort.Slice(temp.Resources, func(i, j int) bool {
+		return temp.Resources[i].ID < temp.Resources[j].ID
+	})
+
+	for id, out := range raw.Outputs {
+		out.ID = id
+		temp.Outputs = append(temp.Outputs, out)
+	}
+	sort.Slice(temp.Outputs, func(i, j int) bool {
+		return temp.Outputs[i].ID < temp.Outputs[j].ID
+	})
+
+	return temp
+}
+
+type argument struct {
+	output   string
+	filename string
+}
+
+func parseArgs(args []string) (argument, error) {
+	arg := argument{}
+	flags := flag.NewFlagSet(args[0], flag.ExitOnError)
+	flags.StringVar(&arg.output, "o", "", "File name. Write to file instead of stdout")
+	flags.Usage = func() {
+		fmt.Fprintf(os.Stdout, "cfn-doc is a tool for generating document of CloudFormation template.\n\n")
+		fmt.Fprintf(os.Stdout, "Usage: \n")
+		fmt.Fprintf(os.Stdout, "  cfn-doc [OPTION] <TEMPLATE FILE>\n\n")
+		fmt.Fprintf(os.Stdout, "Options: \n")
+		flags.PrintDefaults()
+	}
+	err := flags.Parse(args[1:])
+	if err != nil {
+		return argument{}, err
+	}
+
+	arg.filename = flags.Args()[0]
+	return arg, nil
+}
+
+func generateDoc(filename string, cfn cfnTemplate) ([]byte, error) {
+	tplValue := newTemplateValue(filename, cfn)
+	tpl, err := template.New("").Parse(docTemplate)
+	if err != nil {
+		return nil, err
+	}
+
+	out := new(bytes.Buffer)
+	err = tpl.Execute(out, tplValue)
+	return out.Bytes(), err
+}
+
+func readCFnTemplate(filename string) (cfnTemplate, error) {
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		return cfnTemplate{}, err
+	}
+
+	raw := cfnTemplate{}
+	err = yaml.Unmarshal(b, &raw)
+	return raw, err
+}
+
+func main() {
+	arg, err := parseArgs(os.Args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	cfn, err := readCFnTemplate(arg.filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	doc, err := generateDoc(arg.filename, cfn)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if arg.output == "" {
+		fmt.Println(string(doc))
+		return
+	}
+
+	if err := os.WriteFile(arg.output, doc, 0644); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
